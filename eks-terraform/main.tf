2edbdd9d5611901c8a80ece9339719787b61e89d
chimdi247 <Chimdi247@gmail.com>
2026-02-15 17:19:46 +0200

    8th commit

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # IAM Role for EKS Cluster
 # ----------------------------
 resource "aws_iam_role" "master" {
-  name = "chimdi-eks-master1"
+  name = "yaswanth-eks-master1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -39,7 +39,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 # IAM Role for Worker Nodes
 # ----------------------------
 resource "aws_iam_role" "worker" {
-  name = "chimdi-eks-worker1"
+  name = "yaswanth-eks-worker1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -54,7 +54,7 @@ resource "aws_iam_role" "worker" {
 }
 
 resource "aws_iam_policy" "autoscaler" {
-  name = "chimdi-eks-autoscaler-policy1"
+  name = "yaswanth-eks-autoscaler-policy1"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -155,7 +155,7 @@ resource "aws_eks_cluster" "eks" {
   }
 
   tags = {
-    Name        = "chimdi-eks-cluster"
+    Name        = "yaswanth-eks-cluster"
     Environment = "dev"
     Terraform   = "true"
   }
